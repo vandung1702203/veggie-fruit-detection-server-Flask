3e9e8e4588330ceab3dabc1c1ccd4e8164a5eece
Nguyên Khôi <115073243+SuKay712@users.noreply.github.com>
2023-11-17 19:22:46 +0700

    upload model

---
+++ app.py
@@ -0,0 +1,57 @@
+from flask import Flask, render_template, request, redirect, url_for, flash
+import model_predict
+import os
+from werkzeug.utils import secure_filename
+
+
+def create_app():
+    app = Flask(__name__)
+    class_names = model_predict.GetClassNames()
+    UPLOAD_FOLDER = 'static/uploads/'
+    app.secret_key = "secret key"
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+
+    ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+
+    def allowed_file(filename):
+        return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    @app.route('/')
+    def home():
+        return render_template('index.html')
+
+    @app.route('/', methods=['POST'])
+    def upload_image():
+
+        if 'file' not in request.files:
+            flash('No file path')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No image selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            label = model_predict.vegetable_predict(class_names, file)
+            flash('Image successfully uploaded and it is ' + label)
+            os.remove(file_path)
+            return render_template('index.html')
+        else:
+            flash('Allowed image types are - png, jpg, jpeg, gif')
+            return redirect(request.url)
+
+    @app.route('/display/<filename>')
+    def display_image(filename):
+        return redirect(url_for('static', filename='uploads/' + filename), code=301)
+
+    @app.route('/show_pic')
+    def show_images():
+        image_names = os.listdir('./static/uploads')
+        return render_template("gallery.html", image_names=image_names)
+
+    return app
+    # if __name__ == "__main__":
+    #     app.run(debug=True)
---
+++ model_predict.py
@@ -0,0 +1,40 @@
+import numpy as np
+import tensorflow as tf
+import os
+
+from keras.preprocessing.image import ImageDataGenerator
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+model_1 = tf.keras.models.load_model("./final_model_new.h5")
+batch_size = 128
+
+
+def GetClassNames():
+    test_preprocess = ImageDataGenerator(rescale=1 / 255.0)
+    img_size = 224
+    test_data = test_preprocess.flow_from_directory(
+        directory=r"./class_names",
+        target_size=(img_size, img_size),
+        color_mode="rgb",
+        batch_size=batch_size,
+        class_mode="categorical",
+        shuffle=False,  # Set this to False for test data
+        seed=42
+    )
+
+    class_labels = list(test_data.class_indices.keys())
+    return class_labels
+
+
+from PIL import Image
+
+
+def vegetable_predict(class_labels, img_path):
+    img_size = 224
+    img = Image.open(img_path)
+    img = img.resize((img_size, img_size))
+    img = np.array(img) / 255.0  # Rescale the image
+    img = np.expand_dims(img, axis=0)
+    prediction = model_1.predict(img)
+    predicted_label = class_labels[np.argmax(prediction)]
+    return predicted_label
